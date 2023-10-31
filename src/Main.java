a3e9af4a8ea6ab0a5c53c6d335ff5801c52e5bb0
Paula Molina <paulaml2003@gmail.com>
2023-10-31 13:53:08 +0100

    empezando

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Paula");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
